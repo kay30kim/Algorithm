fd1e67d0612d67a8262b1c826d2894eebd9711dd
kay30kim <kay30kim@gmail.com>
2021-06-11 00:07:41 +0900

    11/06/2021 updated

---
+++ python/2. editing text3.py
@@ -0,0 +1,21 @@
+print('안녕하세요')
+a=1
+b=1
+print(a+b)
+_myname = 'kay'
+my_name = '경호'
+MyName2 = 'kay kim'
+count = 1
+counter = 2
+print(count)
+import keyword
+keyword.kwlist
+print(keyword.kwlist)
+number = 1
+print(number+2)
+number = 'one'
+number
+print(number) #주석
+"""a=1
+b=1"""
+
