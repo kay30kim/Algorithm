820d745147d11e069caed8476a235cbeb353b466
kay30kim <kay30kim@gmail.com>
2021-06-11 21:23:04 +0900

    11/06/2021 updated

---
+++ python/8.tab.py
@@ -0,0 +1,26 @@
+a = 200
+msg = 'I love Python'
+list_data = ['a','b','c']
+dic_data = {'a':97, 'b':98}
+print(a)
+print(msg)
+print(list_data)
+print(list_data[0])
+print(dic_data)
+print(dic_data['a'])
+print('#',end="")
+print('#')
+listdata=['a','b','c']
+if 'a' in listdata:
+    print('a 가 listdata에 있습니다.')
+    print(listdata)
+else:
+    print('a가 listdata에 존재하지 않습니다.')
+x=1
+y=2
+if x>y:
+    print('x가 y보다 큽니다.')
+elif x<y:
+    print('x가 y보다 작습니다.')
+else:
+    print('같다')
